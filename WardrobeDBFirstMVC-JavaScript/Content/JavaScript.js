4be7ba81d33df82f8d47c9f12fd923ce0912499a
divnag <divyamn.shine@gmail.com>
2018-03-23 15:30:58 -0400

    searching for specific item working

---
+++ WardrobeDBFirstMVC-JavaScript/Content/JavaScript.js
@@ -0,0 +1,26 @@
+﻿function Greeting() {
+    var x = document.getElementById("jumbotron-titlebox");
+    var userResponse = prompt('Welcome to My Wardrobe . What is your name?');
+    x.getElementsByTagName('h1')[0].innerText = 'Welcome ' + userResponse+'  to';
+}
+
+
+function sort() {
+    var response = prompt("Clothes you would like to search?");
+    var x = document.getElementsByClassName("Season");
+    var y = document.getElementsByClassName("photos");
+    var found=new Array(0);
+
+    for (var i = 0; i < x.length; i++) {
+        if (x[i].innerText == response) {
+            found.push(response);
+            //found = found +" "+ x[i].innerText;
+           }
+        
+    }
+
+    for (var i = 0; i < found.length; i++) {
+         alert(y[i].innerText);
+    }
+    alert(found);
+}
